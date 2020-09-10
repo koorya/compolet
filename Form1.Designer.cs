2d6e0799dd6d8b776e93d011d1fcf9937549c3bd
Kirill Ivanov <puaktopblm@yandex.ru>
2020-09-10 19:11:28 +0700

    добавил немного коментариев в файл создания формы

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -14,9 +14,9 @@ namespace common_compolet_pure
     partial class Form1
     {
 
-        private System.ComponentModel.IContainer components = null;
+        private System.ComponentModel.IContainer components = null; 
 
-        private ComboBox plcName;
+        private ComboBox plcName; //выпадающий список для выбора плк
 
         private void InitializeComponent()
         {
@@ -25,20 +25,24 @@ namespace common_compolet_pure
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Text = "Form1";
             
-            plc_conn = new List<ExtCompolet>();
+            plc_conn = new List<ExtCompolet>(); // список объектов для связи с плк через CIP
 
-            FileStream fs = new FileStream("user.json", FileMode.OpenOrCreate);
+            FileStream fs = new FileStream("user.json", FileMode.OpenOrCreate); // поток для чтения конфгов плк из файла
             
-            ValueTask<List<ExtComp_serial>> _deser = JsonSerializer.DeserializeAsync<List<ExtComp_serial>>(fs);
+            ValueTask<List<ExtComp_serial>> _deser = JsonSerializer.DeserializeAsync<List<ExtComp_serial>>(fs); // десериализация промежуточных объектов, 
+            // по которым будут создаваться обычные объекты для общения с плк
 
-            while(_deser.IsCompleted);
+            while(_deser.IsCompleted);//если прочиталось с успехом, можно продолжать. Потенциально здесь ошибка и проблема.
 
+            // по каждому из прочитанных промежуточных объектов создаем нормальный объект для связи с плк
             foreach (ExtComp_serial deser in _deser.Result)
             {
                 ExtCompolet plc = new ExtCompolet(this.components, deser);
+                // добавляем этот объект в список объектов для связи с плк
                 plc_conn.Add(plc);
             }
 
+            #region serialization to file
             List<ExtComp_serial> ser_list = new List<ExtComp_serial>();
             foreach (ExtCompolet plc in plc_conn)
             {
@@ -55,41 +59,40 @@ namespace common_compolet_pure
                 JsonSerializer.SerializeAsync<List<ExtComp_serial>>(_fs, ser_list, options);
                 Console.WriteLine("Data has been saved to file");
             }
+            #endregion
 
+            // список фреймов с полями для управления конкретным плк
             plc_form_list = new List<PLCForm>();
             foreach(ExtCompolet plc in plc_conn)
             {
-                PLCForm plc_form = new PLCForm(plc);
-                plc_form_list.Add(plc_form);
-                this.Controls.Add(plc_form);
-                plc_form.Visible = false;
+                PLCForm plc_form = new PLCForm(plc); // создали сам фрейм
+                plc_form_list.Add(plc_form); // добавили его в список фреймов, чтоб потом была возможность их все перебрать
+                this.Controls.Add(plc_form); // добавили этот фрейм на форму (окошко)
+                plc_form.Visible = false; // сделали форму невидимой, чтоб они не перекрывались.
             }
 
-            this.plcName = new ComboBox();
-            this.Controls.Add(plcName);
-
-
+            this.plcName = new ComboBox(); // поле для выбора активного фрейма в окне
+            this.Controls.Add(plcName); // добавляем это поле на форму
             this.plcName.Location = new System.Drawing.Point(10, 220);
-
-
             this.plcName.Name = "plcName";
-        //    this.plcName.Size = new System.Drawing.Size(176, 20);
-        //    this.plcName.TabIndex = 0;
+            this.plcName.SelectedIndexChanged += this.plcName_ValueChanged; // устанавливаем обработчик события изменения выбранного индекса
 
-            this.plcName.SelectedIndexChanged += this.plcName_ValueChanged;
-
+            // заполняем этот выпадающий список именами плк, проходя по списку объектов для связи с плк
             foreach (ExtCompolet plc in plc_conn)
             {
                 this.plcName.Items.Add(plc.plc_name);
             }
-            this.plcName.SelectedIndex = 0;
-            plcName_ValueChanged(null, null);
+            this.plcName.SelectedIndex = 0; // выбираем первый пункт в списке, чтобы после у нас отобразился один фрейм
+            plcName_ValueChanged(null, null); // имитируем событие смены индекса, чтобы у нас отобразился первый фрейм
 
 
         }
 
+        // обработчик события при выборе текущего плк
 		private void plcName_ValueChanged(object sender, System.EventArgs e)
 		{
+            // проходим по списку фреймов и гасим все, у которых номер 
+            // не совпадает с выбранным пунктом. А выббранный наоборот делаем видимым
             int i = 0;
             foreach(PLCForm plc_form in plc_form_list)
             {
--- IPLCConnect.cs
+++ IPLCConnect.cs
@@ -1,6 +1,9 @@
 
 namespace common_compolet_pure
 {
+    // этот интерфейс должен быть у всех объектов, которые 
+    // обеспечивают связь с контроллером и позволяют читать 
+    // и писать их переменные
     public interface IPLCConnect 
     {
         object WriteVar(string name, object value);
